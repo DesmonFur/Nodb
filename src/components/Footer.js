5ccdff77978d401a57d77c59cfeb5d80e6137a75
DesmondFur <desfurtick@gmail.com>
2019-07-22 15:51:45 -0600

    Danzo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Quote from './Quote'
 export default function Footer() {
   return (
     <div className="footer">
       <h1>BOARD Of Skateboards</h1>
+      <Quote />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Quote from './Quote'
 export default function Header() {
   return (
     <div className="header">
       <h1>BOARD OF SKATEBOARDS</h1>
+      <Quote />
       <img
         src="https://media2.giphy.com/media/8jYbxR1QN6p68/200w.webp?cid=790b76115d32419e67425a662e6ff870&rid=200w.webp"
         alt="Jesus skates with me"
